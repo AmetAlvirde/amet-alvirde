8f5bbf03ba93e2f8315eb2f54049b7cfbad70e2a
Amet Alvirde <Amet.Alvirde@gmail.com>
2026-03-05 17:44:48 -0300

    refactor: Works on accesibility tests

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -1,12 +1,11 @@
+// disabled assertion categories are not relevant for this site as is.
+// We want to do a 1:1 to the web audit experience, meaning assesing only performance, accessibility, best practices and SEO.
 module.exports = {
   ci: {
     collect: {
       startServerCommand: "pnpm build && pnpm preview --port 4173",
-      url: [
-        "http://localhost:4173/",
-        "http://localhost:4173/writing/"
-      ],
-      numberOfRuns: 1
+      url: ["http://localhost:4173/", "http://localhost:4173/writing/"],
+      numberOfRuns: 1,
     },
     assert: {
       preset: "lighthouse:recommended",
@@ -14,12 +13,14 @@ module.exports = {
         "categories:performance": ["warn", { minScore: 0.9 }],
         "categories:accessibility": ["error", { minScore: 1.0 }],
         "categories:best-practices": ["warn", { minScore: 0.95 }],
-        "categories:seo": ["warn", { minScore: 0.95 }]
-      }
+        "categories:seo": ["warn", { minScore: 0.95 }],
+        "network-dependency-tree-insight": "off",
+        "render-blocking-insight": "off",
+        "render-blocking-resources": "off",
+      },
     },
     upload: {
-      target: "temporary-public-storage"
-    }
-  }
+      target: "temporary-public-storage",
+    },
+  },
 };
-
